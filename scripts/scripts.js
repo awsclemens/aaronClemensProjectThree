c83ca264b12588d7fb122ab396da67e214b8e85d
Aaron <aws.clemens@gmail.com>
2020-09-08 17:42:43 -0600

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+// namespace object
+const photoPuzzle = {};
+
+// define init method
+photoPuzzle.init = function() {
+    console.log("initialized");
+}
+
+// document ready
+$(document).ready(function() {
+    photoPuzzle.init();
+});
